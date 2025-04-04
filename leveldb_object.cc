7c5fa166e56ebf37856a098a23a8101c83c3017d
Fer Paulovsky <fpaulovsky@encircleapp.com>
2025-04-04 10:50:22 -0300

    Re-add support for python 2.x

--- leveldb_object.cc
+++ leveldb_object.cc
@@ -783,6 +783,12 @@ static PyMethodDef PyLevelDBSnapshot_methods[] = {
 
 static int pyleveldb_str_eq(PyObject* p, const char* s)
 {
+	// 8-bit string
+	#if PY_MAJOR_VERSION < 3
+	if (PyString_Check(p) && strcmp(PyString_AS_STRING(p), "bytewise") == 0)
+		return 1;
+	#endif
+	
 	// unicode string
 	if (PyUnicode_Check(p)) {
 		Py_ssize_t size;
